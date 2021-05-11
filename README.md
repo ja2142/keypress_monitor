719675c7df9b8f1d16fa0841b361f96d5985d75a
Radoslaw Olko <radoslaw.olko@gmail.com>
2021-05-11 18:57:51 +0200

    add everything backened related in python
    now it: creates temp directory, creates the pipe, launches the logkeys
    and destroys the pipe once the main process is done, instead of expecting all
    this to be done externally

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
 # QKL - Qt Key Logger
 
-## Running with function keys
+Depends on [logkeys](https://github.com/kernc/logkeys).
+Requires root to launch logkeys.
+
+Terminating the logkeys process might not be fully reliable.
+For that, and other reasons I'd advise against using it on any machine on which
+any kinds of secrets are being input (in general maybe don't run a keylogger on a machine you care about).
+
+### Running with function keys
 
 ```bash
-# firstly create fifo
-mkfifo keylogger_pipe
-# start keylogging
-./logkeys_with_func
-# start gui
 ./main.py
 ```
 
-## Running without function keys
+### Running without function keys
 
 ```bash
-# firstly create fifo
-mkfifo keylogger_pipe
-# start keylogging
-./logkeys
-# start gui
 ./main.py --no-func-keys
 ```
+
+Close the app with alt+F4.
 
--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python3
 
-import sys
 import os
-import time
+import re
+import shlex
+import subprocess
+import sys
+import tempfile
 import threading
+import time
 
 from PyQt5.QtCore import QObject, pyqtProperty, pyqtSignal
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 
+def launch_backend(no_func_keys=False):
+    tmp_dir = tempfile.TemporaryDirectory()
+    pipe_filename = tmp_dir.name+'/keylogger_pipe'
+    os.mkfifo(pipe_filename)
+    print(f'fifo created at {pipe_filename}')
+    os.chmod(pipe_filename, 0o600)
+    logkeys_cmd = f'logkeys -s --no-daemon --keymap keymap --no-timestamps --output={pipe_filename}'
+    if no_func_keys:
+        logkeys_cmd += ' --no-func-keys'
+    return (subprocess.Popen(shlex.split(logkeys_cmd)), tmp_dir)
 
 class KeyPressProvider(QObject):
     keysPressedChanged = pyqtSignal(str)
 
-    def __init__(self, parent=None, ignore_keys=[]):
+    def __init__(self, parent=None, ignore_keys=[], no_func_keys=False):
         super(KeyPressProvider, self).__init__()
+        self.keylogger_process, self.tmp_dir = launch_backend(no_func_keys)
+        self.pipe_filename = self.tmp_dir.name + '/keylogger_pipe'
         self.keys_pressed = ''
-        self.ignore_keys = ignore_keys
+        # ignore backsapce, bacause it's emulated in clearFuncKeys
+        self.ignore_keys = ignore_keys + ['<BckSp>']
 
         self.read_thread = threading.Thread(target=self.read)
         # the daemon thread will be terminated once main thread dies
@@ -24,11 +41,14 @@ class KeyPressProvider(QObject):
         self.read_thread.start()
 
     def clearFuncKeys(self):
+        # remove letters before backspace first, then if any leading <BckSp> remains
+        # it will be removed through self.ignore_keys
+        self.keys_pressed = re.sub('.<BckSp>', '', self.keys_pressed)
         for function_key in self.ignore_keys:
             self.keys_pressed = self.keys_pressed.replace(function_key, '')
 
     def read(self):
-        with open('keylogger_pipe', 'r') as keylogger_pipe:
+        with open(self.pipe_filename, 'r') as keylogger_pipe:
             while True:
                 new_char = keylogger_pipe.read(1)
                 if new_char == '':
@@ -42,13 +62,19 @@ class KeyPressProvider(QObject):
                 self.keysPressedChanged.emit(self.keys_pressed)
         print('keylogger_pipe closed, reading thread terminating')
 
-if __name__ == "__main__":
-    app = QGuiApplication(sys.argv[:1])
-    if '--no-func-keys' in sys.argv:
+def main():
+    if os.getuid() != 0:
+        print('root permissions needed to launch keylogger')
+        return
+    no_func_keys = '--no-func-keys' in sys.argv
+    if no_func_keys:
         ignored_keys = []
     else:
         ignored_keys=['<Enter>', '<LShft>', '<#+8>', '<#+18>']
-    keypressprovider = KeyPressProvider(ignore_keys=ignored_keys)
+
+    app = QGuiApplication(sys.argv[:1])
+
+    keypressprovider = KeyPressProvider(ignore_keys=ignored_keys, no_func_keys=no_func_keys)
 
     engine = QQmlApplicationEngine()
     engine.rootContext().setContextProperty("text_provider", keypressprovider)
@@ -61,3 +87,6 @@ if __name__ == "__main__":
 
     k.connect(window.setText)
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
